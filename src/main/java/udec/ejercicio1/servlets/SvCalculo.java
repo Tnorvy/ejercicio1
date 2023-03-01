ba333ec80863cf8838614cc873862ec65b45c318
Tnorvy <gabrielemartinez@ucundinamarca.edu.co>
2023-03-01 09:33:36 -0500

    creacion servlet
    clase de java donde se haran los calculos

---
+++ src/main/java/udec/ejercicio1/servlets/SvCalculo.java
@@ -0,0 +1,35 @@
+package udec.ejercicio1.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "SvCalculo", urlPatterns = {"/SvCalculo"})
+public class SvCalculo extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
